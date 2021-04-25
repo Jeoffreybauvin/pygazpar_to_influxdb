43043a7decfb49ed7075c11dd9b99ee2fdafbc0c
Philippe <philippe.branly@gmail.com>
2021-04-25 19:34:07 +0200

    Update pygazpar_to_influxdb.py

--- pygazpar_to_influxdb.py
+++ pygazpar_to_influxdb.py
@@ -67,8 +67,8 @@ for measure in data:
             "start_index_m3": measure['start_index_m3'],
             "end_index_m3": measure['end_index_m3'],
             "energy_kwh": measure['energy_kwh'],
-            "converter_factor_kwh/m3": float(measure['converter_factor_kwh/m3'].replace(',', '.')),
-            "temperature_degC": float(measure['temperature_degC'].replace(',', '.')),
+            "converter_factor_kwh/m3": measure['converter_factor_kwh/m3'],
+            "temperature_degC": measure['temperature_degC'],
             "type": measure['type'],
         }
     })
