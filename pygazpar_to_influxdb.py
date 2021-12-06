8a71463d221bbe6d318adb89c4a9162f85164f31
Philippe <philippe.branly@gmail.com>
2021-12-06 09:32:34 +0100

    Update pygazpar_to_influxdb.py

--- pygazpar_to_influxdb.py
+++ pygazpar_to_influxdb.py
@@ -70,7 +70,7 @@ write_api = influxclient.write_api(write_options=SYNCHRONOUS)
 
 #------------------------------------------------- 
         
-client = pygazpar.Client(login=login, password=password, pce=pce, pygazpar.Frequency.DAILY, 30, '/tmp')
+client = pygazpar.Client(usernamelogin, password=password, pceIdentifier=pce, meterReadingFrequency=Frequency.DAILY, lastNDays=30, tmpDirectory='/tmp')
 
 log.debug('Starting to update pygazpar data')
 client.update()
