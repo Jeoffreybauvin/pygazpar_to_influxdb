08d6c4d5b1aeca16655de3757b3c93228b6a1a69
Jeoffrey Bauvin <jeoffrey54@gmail.com>
2022-03-08 16:43:28 +0100

    new version

--- pygazpar_to_influxdb.py
+++ pygazpar_to_influxdb.py
@@ -2,7 +2,9 @@
 
 import sys
 import json
-from influxdb import InfluxDBClient
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+from pygazpar.enum import Frequency
 import datetime
 import argparse
 import logging
@@ -10,19 +12,27 @@ import re
 import decimal
 from decimal import Decimal
 import pygazpar
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url_influxdb = os.environ['PYGAZPAR_INFLUXDB2_HOST']
+bucket_influxdb = os.environ['PYGAZPAR_INFLUXDB2_BUCKET']
+token_influxdb = os.environ['PYGAZPAR_INFLUXDB2_TOKEN']
+org_influxdb = os.environ['PYGAZPAR_INFLUXDB2_ORG']
+
+login_pygazpar = os.environ['PYGAZPAR_PYGAZPAR_LOGIN']
+password_pygazpar = os.environ['PYGAZPAR_PYGAZPAR_PASSWORD']
+pce_pygazpar = os.environ['PYGAZPAR_PCE_IDENTIFIER']
+pce_lastNDays = int(os.environ['PYGAZPAR_LASTNDAY'])
 
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--source", help="Source ('json' file must be named data.json. 'pygazpar' asks to pygazpar to retrieve data)", dest="SOURCE", default="pygazpar")
-parser.add_argument("--influxdb-host", help="InfluxDB host", dest="INFLUXDB_HOST", default="influxdb-api.loc")
-parser.add_argument("--influxdb-port", help="InfluxDB port", dest="INFLUXDB_PORT", default=8086)
-parser.add_argument("--influxdb-username", help="InfluxDB username", dest="INFLUXDB_USERNAME", default="username")
-parser.add_argument("--influxdb-password", help="InfluxDB password", dest="INFLUXDB_PASSWORD", default="password")
-parser.add_argument("--influxdb-database", help="InfluxDB database", dest="INFLUXDB_DATABASE", default="enedis")
+
 parser.add_argument("-v", "--verbose", dest="verbose_count", action="count", default=0, help="increases log verbosity")
-parser.add_argument("--pygazpar-login", dest="PYGAZPAR_LOGIN", help="pygazpar login")
-parser.add_argument("--pygazpar-password", dest="PYGAZPAR_PASSWORD", help="pygazpar password")
 
 args = parser.parse_args()
 log = logging.getLogger()
@@ -30,21 +40,15 @@ logging.basicConfig(stream=sys.stderr, level=logging.WARNING,
                     format='%(name)s (%(levelname)s): %(message)s')
 log.setLevel(max(3 - args.verbose_count, 0) * 10)
 
-influx_client = InfluxDBClient(
-    host=args.INFLUXDB_HOST,
-    port=args.INFLUXDB_PORT,
-    username=args.INFLUXDB_USERNAME,
-    password=args.INFLUXDB_PASSWORD,
-    database=args.INFLUXDB_DATABASE,
-    timeout=5,
-    retries=2,
-)
 
-client = pygazpar.Client(args.PYGAZPAR_LOGIN,
-                         args.PYGAZPAR_PASSWORD,
-                         'geckodriver',
-                         30,
-                         '/tmp')
+influxclient = InfluxDBClient(url=url_influxdb, token=token_influxdb, org=org_influxdb)
+
+
+write_api = influxclient.write_api(write_options=SYNCHRONOUS)
+
+#------------------------------------------------- 
+        
+client = pygazpar.Client(username=login_pygazpar, password=password_pygazpar, pceIdentifier=pce_pygazpar, meterReadingFrequency=Frequency.DAILY, lastNDays=pce_lastNDays, tmpDirectory='/tmp')
 
 log.debug('Starting to update pygazpar data')
 client.update()
@@ -57,22 +61,25 @@ jsonInflux = []
 for measure in data:
     print(measure)
     date_time_obj = datetime.datetime.strptime(measure['time_period'], '%d/%m/%Y')
-
     if 'start_index_m3' in measure:
       jsonInflux.append({
-          "measurement": "gazpar_consumption_per_day",
-          "tags": {
-          },
-          "time": date_time_obj.strftime('%Y-%m-%dT%H:%M:%S'),
-          "fields": {
-              "value": measure['volume_m3'],
-              "start_index_m3": measure['start_index_m3'],
-              "end_index_m3": measure['end_index_m3'],
-              "energy_kwh": measure['energy_kwh'],
-              "type": measure['type'],
-          }
-      })
+        "measurement": "gazpar_consumption_per_day",
+        "tags": {
+        "year": date_time_obj.strftime("%Y"),
+        "month": int(date_time_obj.strftime("%m")),
+        },
+        "time": date_time_obj.strftime('%Y-%m-%dT%H:%M:%S'),
+        "fields": {
+            "value": measure['volume_m3'],
+            "start_index_m3": measure['start_index_m3'],
+            "end_index_m3": measure['end_index_m3'],
+            "volume_m3": measure['volume_m3'],
+            "energy_kwh": measure['energy_kwh'],
+            "converter_factor_kwh/m3": measure['converter_factor_kwh/m3'],
+            "type": measure['type'],
+        }
+    })
     else:
       print('No measure')
 
-influx_client.write_points(jsonInflux, batch_size=10)
+write_api.write(bucket=bucket_influxdb, record=jsonInflux)
